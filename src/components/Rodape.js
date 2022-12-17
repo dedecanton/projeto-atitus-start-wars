045f80f8a2e8eaaa10133a0e7d05835ee78a8188
André Canton <andrecanton18@gmail.com>
2022-12-17 12:52:11 -0300

    feat: requirement 4

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -6,6 +6,8 @@ export default function Rodape() {
             <span>Desenvolvido na Disciplina de Tópicos Especiais em Programação</span>
             <br />
             <span>@Copyright 2022 - ATITUS </span>
+            <br />
+            <span>André Luís Lorini Canton - RA: 1129468 - andrecanton18@gmail.com</span>
         </div>
     )
 }
