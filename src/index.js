62cee097d98282bf0659c695863267b9d4d45053
André Canton <andrecanton18@gmail.com>
2022-12-18 16:40:28 -0300

    feat: toastify elements and contexts

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 import { LoginContextProvider } from './contexts/LoginContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ContadorProvider } from './contexts/ContadorContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <LoginContextProvider>
-      <App />
+      <ContadorProvider>
+        <ToastContainer />
+        <App />
+      </ContadorProvider>
     </LoginContextProvider>
   </React.StrictMode>
 );
