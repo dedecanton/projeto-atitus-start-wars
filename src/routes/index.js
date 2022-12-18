35ed65ee3ab40d4d3753355c99d7738329fc28ab
André Canton <andrecanton18@gmail.com>
2022-12-18 16:40:04 -0300

    feat: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
 import Login from '../pages/Login';
 import Menu from '../pages/Menu';
-import Cadastro from '../pages/Cadastro';
 import NotFound from '../pages/NotFound';
+
+import Home from '../components/Home';
+import Films from '../components/Films';
+import Starships from '../components/Starships';
+import People from '../components/People';
+import Planets from '../components/Planets';
+import Species from '../components/Species';
+import Questions from '../components/Questions';
+import About from '../components/About';
 
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
@@ -9,9 +17,18 @@ export const PrivateRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Navigate to="/menu" />} />
-      <Route path="/menu" element={<Menu />} />
-      <Route path="/cadastro" element={<Cadastro />} />
-      <Route path="*" element={<NotFound/>} />
+      <Route path="/menu" element={<Menu />}>
+        <Route index element={<Home />} />
+        <Route path="films" element={<Films />} />
+        <Route path="starships" element={<Starships />} />
+        <Route path="people" element={<People />} />
+        <Route path="planets" element={<Planets />} />
+        <Route path="species" element={<Species />} />
+        <Route path="duvidas" element={<Questions />} />
+        <Route path="sobre" element={<About />} />
+        <Route path="*" element={<h1>Menu não encontrado</h1>} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );
@@ -21,8 +38,7 @@ export const PublicRoutes = () => (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/menu" element={<Navigate to="/" />} />
-      <Route path="/cadastro" element={<Navigate to="/" />} />
-      <Route path="*" element={<NotFound/>} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );
