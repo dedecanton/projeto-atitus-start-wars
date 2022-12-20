c30d52b276a5a57783212f13ae4dfcf685042011
André Canton <andrecanton18@gmail.com>
2022-12-20 20:39:16 -0300

    fix: adjust validation with count

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -8,6 +8,8 @@ export default function Rodape() {
             <span>@Copyright 2022 - ATITUS </span>
             <br />
             <span>André Luís Lorini Canton - RA: 1129468 - andrecanton18@gmail.com</span>
+            <br />
+            <span>Fabricio Mesomo - RA: 1128500 - fabricio.mezomo10@gmail.com</span>
         </div>
     )
 }
